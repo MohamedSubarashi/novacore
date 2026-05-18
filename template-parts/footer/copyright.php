2a9bab054d1ec55bd7736f52cabf7c37137977c6
mohamedsubarashi <mohamedsubarashi@gmail.com>
2026-05-18 13:14:10 +0100

    Simplify footer copyright to single GPL notice line

--- template-parts/footer/copyright.php
+++ template-parts/footer/copyright.php
@@ -7,32 +7,11 @@
  */
 
 defined( 'ABSPATH' ) || exit;
-
-if ( ! function_exists( 'dynamic_footer_copyright' ) ) {
-    function dynamic_footer_copyright( $prefix = '' ) {
-        $year       = gmdate( 'Y' );
-        $site_name  = get_bloginfo( 'name' );
-        $site_link  = '<a href="' . esc_url( home_url( '/' ) ) . '" class="novacore-footer__site-name">' . esc_html( $site_name ) . '</a>';
-        $theme_name = wp_get_theme()->get( 'Name' );
-        $tagline    = get_theme_mod( 'novacore_copyright_tagline', esc_html__( 'All rights reserved', 'novacore' ) );
-        return $prefix . "Copyright &copy; $year | $site_link | Powered by $theme_name" . " - " . $tagline;
-    }
-}
 ?>
 <div class="novacore-footer__copyright">
-	<?php echo wp_kses_post( dynamic_footer_copyright() ); ?>
-	<br>
-	<?php
-	printf(
-		/* translators: 1: Theme name, 2: Copyright year, 3: Author name, 4: GPL license link */
-		esc_html__( '%1$s WordPress Theme, Copyright (C) %2$s %3$s. %1$s is distributed under the terms of the %4$s.', 'novacore' ),
-		esc_html( wp_get_theme()->get( 'Name' ) ),
-		gmdate( 'Y' ),
-		esc_html( wp_get_theme()->get( 'Author' ) ),
-		sprintf(
-			'<a href="%s" rel="license">GNU GPL v3 or later</a>',
-			esc_url( wp_get_theme()->get( 'License URI' ) )
-		)
-	);
-	?>
+	Copyright &copy; <?php echo gmdate( 'Y' ); ?>
+	| <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+	| NovaCore WordPress Theme <?php echo esc_html( wp_get_theme()->get( 'Author' ) ); ?>
+	is distributed under the
+	<a href="<?php echo esc_url( wp_get_theme()->get( 'License URI' ) ); ?>" rel="license">GNU GPL License</a>.
 </div>
